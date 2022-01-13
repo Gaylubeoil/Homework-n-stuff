a1fc781d9ef225d276c741627424cf5257310dbc
Gaylubeoil <92313524+Gaylubeoil@users.noreply.github.com>
2022-01-13 22:05:45 +0200

    Create Task03.cpp

---
+++ HW02/Task03.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+const int maxsize = 256;
+void sort1(char* str) {
+	char arr[maxsize];
+	int index = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '1' || str[i] == '0' || str[i] == '&' || str[i] == '|' || str[i] == '=') {
+			arr[index] = str[i];
+			index++;
+		}
+	}
+	bool flag = false;
+	for (int i = 0; i < index; i++)
+		std::cout << arr[i];
+
+    for (int j = 0, i = 1; j < index, i < index - 2; j += 2, i += 2) {
+	if ((arr[j] == '0' || arr[j] == '1') && (arr[i] == '|' || arr[i] == '&') && (arr[index - 2] == '=')) flag = true;
+	else flag = false;
+    }
+std::cout << std::endl << "Boolean expression is " << (flag ? "valid!" : "not valid!") << std::endl;
+}
+int main() {
+	char str1[maxsize];
+	std::cin.getline(str1, maxsize);
+	sort1(str1);
+	return 0;
+}
